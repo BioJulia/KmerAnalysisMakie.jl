7fb71007fb7a9197012a2bc25bf1746047dd1d75
Ben J. Ward <benjward@protonmail.com>
2020-07-05 15:35:31 +0100

    Update spectracn.jl

--- src/full_recipes/spectracn.jl
+++ src/full_recipes/spectracn.jl
@@ -17,7 +17,7 @@ function AbstractPlotting.plot!(plot::SpectraCN)
     # Lower to multiple barplots/plots here.
     spct = to_value(plot[:spectra])
     heights = cumsum(_find_plottable_subset(spct); dims = 2)
-    @inbounds for col in size[2]:-1:1
+    @inbounds for col in size(heights)[2]:-1:1
         barplot!(plot, [Point2f0(i - 1, j) for (i, j) in enumerate(heights[:,col])], color = CN_COLOUR[col])
     end
     # TODO: Figure out how to add a colour legend and perhaps axis labels.
